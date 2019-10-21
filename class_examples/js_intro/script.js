984981e317bfb03a040a38f805dc8b42fabeaa84
chuyunwei97 <cxw664@miami.edu>
2019-10-21 13:28:59 -0400

    javascript

---
+++ class_examples/js_intro/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    // all your javascript goes here
+    console.log("Everything loaded!");
+    console.log("this function ran");
+    // "+" for string is putting two strings together
+    var x = 2+"2";
+    var y = 2+2;
+    var z;
+    // defining a variable
+    z = "My name is A";
+    console.log(x);
+});
+
+
+// var array1 = [1,2,3,4,5,6];
+// var array2 = ["This is a string","This is also a string"];
+// var x = 4;
+// var heading = document.getElementsByTagName("h1");
+// console.log(heading);
+// console.log(x);
